41bd97ae293ccda71d1e715a804d95cbe882dd29
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-19 21:43:22 -0600

    Finished the requirements! Except for the tests..

--- src/app/star-wars/planet-list/planet-list.component.ts
+++ src/app/star-wars/planet-list/planet-list.component.ts
@@ -1,53 +1,33 @@
 import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
-// Might not need input after all.
 
 @Component({
   selector: 'app-planet-list',
   templateUrl: './planet-list.component.html',
   styleUrls: ['./planet-list.component.scss']
 })
-@Injectable()
 export class PlanetListComponent implements OnInit {
-  planetArr: Array<any> = [];
   @Input() planetName: string | undefined;
 
-  // Communicate between components
-  toggleDetails: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  
-
-// I may just do all of that bizz in here then.
+// I'm leaving the HttpClient on for testing purposes. TODO delete when finished
   constructor(private http: HttpClient) {
-    this.getPlanets().subscribe((planets: any) => {
-      // populate array with planets.
-      planets.results.forEach((element: any) => {
-        this.planetArr.push(element);
-      });
-      // Here we're sorting the array of objects by name.
-      this.planetArr.sort((a, b) => a.name.localeCompare(b.name));
+    // this.getPlanets().subscribe((planets: any) => {
+    //   // populate array with planets.
+    //   planets.results.forEach((element: any) => {
+    //     this.planetArr.push(element);
+    //   });
+    //   // Here we're sorting the array of objects by name.
+    //   this.planetArr.sort((a, b) => a.name.localeCompare(b.name));
 
-    });
+    // });
    }
 
-   showDetails() {
-    // let clickedText = 
-    console.log("showing up here");
-    // let details = document.getElementById("details");
-    // details?.classList.toggle('hidden');
-    // console.log(details);
-
-    // this.toggleDetails = !this.toggleDetails;
-   }
-
-   getPlanets(): Observable<any> {
-    const planetApiUrl = 'https://swapi.dev/api/planets';
-    return this.http.get(planetApiUrl);
-  }
+  //TODO Delete when done
+  //  getPlanets(): Observable<any> {
+  //   const planetApiUrl = 'https://swapi.dev/api/planets';
+  //   return this.http.get(planetApiUrl);
+  // }
 
   ngOnInit(): void {
   }
--- src/app/star-wars/planets/planets.component.ts
+++ src/app/star-wars/planets/planets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { PlanetsService } from 'src/app/star-wars/planets.service';
@@ -12,8 +12,16 @@ export class PlanetsComponent implements OnInit {
   // I'm going to go the route of a bunch of variables...
   planetArr: Array<any> = [];
   // I also may go this hideous route. Ah!
+  public show0:boolean = false;
   public show1:boolean = false;
   public show2:boolean = false;
+  public show3:boolean = false;
+  public show4:boolean = false;
+  public show5:boolean = false;
+  public show6:boolean = false;
+  public show7:boolean = false;
+  public show8:boolean = false;
+  public show9:boolean = false;
 
 
   //first planet 
@@ -25,72 +33,155 @@ export class PlanetsComponent implements OnInit {
   planet1: string | undefined;
   planetKeysArr1: Array<any> = [];
   planetValsArr1: Array<any> = [];
+
+  // third planet
+  planet2: string | undefined;
+  planetKeysArr2: Array<any> = [];
+  planetValsArr2: Array<any> = [];
+
+  // fourth planet
+  planet3: string | undefined;
+  planetKeysArr3: Array<any> = [];
+  planetValsArr3: Array<any> = [];
+
+  // fifth planet
+  planet4: string | undefined;
+  planetKeysArr4: Array<any> = [];
+  planetValsArr4: Array<any> = [];
+
+  // sixth planet
+  planet5: string | undefined;
+  planetKeysArr5: Array<any> = [];
+  planetValsArr5: Array<any> = [];
+
+  // seventh planet
+  planet6: string | undefined;
+  planetKeysArr6: Array<any> = [];
+  planetValsArr6: Array<any> = [];
+
+  // eigth planet
+  planet7: string | undefined;
+  planetKeysArr7: Array<any> = [];
+  planetValsArr7: Array<any> = [];
+
+  // ninth planet
+  planet8: string | undefined;
+  planetKeysArr8: Array<any> = [];
+  planetValsArr8: Array<any> = [];
+
+  // tenth planet
+  planet9: string | undefined;
+  planetKeysArr9: Array<any> = [];
+  planetValsArr9: Array<any> = [];
+
   constructor(private planetsService: PlanetsService, private http: HttpClient) {
-    // 
     this.getPlanets().subscribe((planets: any) => {
       // populate array with planets.
       planets.results.forEach((element: any) => {
         this.planetArr.push(element);
       });
-
-      // Sort
+      // Sort alphabetically
       this.planetArr.sort((a, b) => a.name.localeCompare(b.name));
-      // Testing inputs
+      // Console log, and potentially include a test here to prove it's alphabetical.
       console.log(this.planetArr);
-
 
       // Start of planet names
       this.planet0 = this.planetArr[0].name;
       this.planet1 = this.planetArr[1].name;
+      this.planet2 = this.planetArr[2].name;
+      this.planet3 = this.planetArr[3].name;
+      this.planet4 = this.planetArr[4].name;
+      this.planet5 = this.planetArr[5].name;
+      this.planet6 = this.planetArr[6].name;
+      this.planet7 = this.planetArr[7].name;
+      this.planet8 = this.planetArr[8].name;
+      this.planet9 = this.planetArr[9].name;
 
-      // Start of planet details
+      // Start of planet details | Prolly should make method to make this cleaner.
       for (const [key, val] of Object.entries(this.planetArr[0])) {
-        // console.log('val ' + val);
         this.planetKeysArr0.push(key);
         this.planetValsArr0.push(val);
       }
       for (const [key, val] of Object.entries(this.planetArr[1])) {
-        // console.log('val ' + val);
         this.planetKeysArr1.push(key);
         this.planetValsArr1.push(val);
       }
-
-
-
-      // planetArr is an array of objects.
-      for (const el of this.planetArr) {
-        // console.log(el.name);
-
-        // const values = Object.values(el);
-        // use values array or:
-        // console.log(values);
-        // for (const val of Object.values(el)) {
-        // console.log(val);
-        // }
-
-        // We have 11 important key/vals in the planet object.
-        for (const [key, val] of Object.entries(el)) {
-          // console.log(key);
-          // console.log(val);
-          // Maybe now, we create variables that hold the key and values for the planets. 
-        }
+      for (const [key, val] of Object.entries(this.planetArr[2])) {
+        this.planetKeysArr2.push(key);
+        this.planetValsArr2.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[3])) {
+        this.planetKeysArr3.push(key);
+        this.planetValsArr3.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[4])) {
+        this.planetKeysArr4.push(key);
+        this.planetValsArr4.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[5])) {
+        this.planetKeysArr5.push(key);
+        this.planetValsArr5.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[6])) {
+        this.planetKeysArr6.push(key);
+        this.planetValsArr6.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[7])) {
+        this.planetKeysArr7.push(key);
+        this.planetValsArr7.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[8])) {
+        this.planetKeysArr8.push(key);
+        this.planetValsArr8.push(val);
+      }
+      for (const [key, val] of Object.entries(this.planetArr[9])) {
+        this.planetKeysArr9.push(key);
+        this.planetValsArr9.push(val);
       }
     });
    }
 
 
-   toggle()  {
-    console.log("showing up here");
-    // Now here we will make our details show from the planet-detail.components.
+   toggle0()  {
+    this.show0 = !this.show0;
+   }
+
+   toggle1()  {
     this.show1 = !this.show1;
    }
 
    toggle2()  {
-    console.log("showing up here");
-    // Now here we will make our details show from the planet-detail.components.
     this.show2 = !this.show2;
    }
 
+   toggle3()  {
+    this.show3 = !this.show3;
+   }
+
+   toggle4()  {
+    this.show4 = !this.show4;
+   }
+
+   toggle5()  {
+    this.show5 = !this.show5;
+   }
+
+   toggle6()  {
+    this.show6 = !this.show6;
+   }
+
+   toggle7()  {
+    this.show7 = !this.show7;
+   }
+
+   toggle8()  {
+    this.show8 = !this.show8;
+   }
+
+   toggle9()  {
+    this.show9 = !this.show9;
+   }
+   
   getPlanets(): Observable<any> {
     const planetApiUrl = 'https://swapi.dev/api/planets';
     return this.http.get(planetApiUrl);
