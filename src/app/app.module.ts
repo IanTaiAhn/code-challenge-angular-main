26d19750c017b6df502c5098c5e8e543da5d62d6
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-22 16:40:52 -0600

    Added an animation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StarWarsModule } from 'src/app/star-wars/star-wars.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     AppRoutingModule,
     StarWarsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/star-wars/planets/planets.component.ts
+++ src/app/star-wars/planets/planets.component.ts
@@ -2,11 +2,31 @@ import { Component, OnInit} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { PlanetsService } from 'src/app/star-wars/planets.service';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('1s ease-in', style({ opacity: 1
+  })),
+]);
+const fadeIn = trigger('fadeIn', [enterTransition]);
+
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1,
+  }),
+  animate('1s ease-out', style({ opacity: 0
+  })),
+]);
+const fadeOut = trigger('fadeOut', [exitTransition]);
 
 @Component({
   selector: 'app-planets',
   templateUrl: './planets.component.html',
-  styleUrls: ['./planets.component.scss']
+  styleUrls: ['./planets.component.scss'],
+  animations: [fadeIn, fadeOut],
 })
 export class PlanetsComponent implements OnInit {
   // I'm going to go the route of a bunch of variables...
