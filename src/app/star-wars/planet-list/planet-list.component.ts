ea45e30f60ca215ee3da87aaa14609fa6a46b143
IanTaiAhn <iantaiahn@yahoo.com>
2022-12-13 21:02:48 -0700

    It has been cleaned up substantially!

--- src/app/star-wars/planet-list/planet-list.component.ts
+++ src/app/star-wars/planet-list/planet-list.component.ts
@@ -9,18 +9,47 @@ import { PlanetsService } from 'src/app/star-wars/planets.service';
 export class PlanetListComponent {
   planetName: Array<any> = [];
   flagArray: Array<any> = [];
+  filmApiArr: Array<any> = [];
 
   constructor(private planetsService: PlanetsService) {
     planetsService.getPlanets().subscribe((planets: any) => {
       this.planetName = planets.results;
       this.planetName.sort((a, b) => a.name.localeCompare(b.name));
-      console.log(this.planetName[0].films[0])
-      // planetsService.getFilms(this.planetName[0].films)
-      let strTest = planetsService.getFilms(this.planetName[0].films[0]);
-      console.log(strTest);
-      console.log(planetsService.getFilms(this.planetName[0].films[0]));
+
+      this.planetName.forEach((el => {
+        // console.log(el.films);
+        let resArr = el.residents
+        // console.log(resArr);
+        let filmArr = el.films;
+        el.films = "";
+        el.residents = "";
+        filmArr.forEach(((film: any) => {
+          // console.log(film)
+          planetsService.getFilms(film).subscribe((item: any) => {
+            // console.log(item.title);
+            el.films += item.title + ", ";
+          })
+        }));
+        resArr.forEach(((resident: any) => {
+          // console.log(resident)
+          planetsService.getFilms(resident).subscribe((item: any) => {
+            // console.log(item);
+            el.residents += item.name + ", ";
+          })
+        }));
+
+      }));
+
     });
-    
+    // console.log(planetsService.getFilms('https://swapi.dev/api/films/1/'));
+    // planetsService.getFilms(this.filmApiArr[0]).subscribe((film: any) => {
+    //   console.log(film.title);
+    // })
+
+    // planetsService.getFilms('https://swapi.dev/api/films/5/').subscribe((film: any) => {
+    //   console.log(film.title);
+    // })
+
     // I can try to change the contents of planetName here?
   }
   
@@ -29,6 +58,5 @@ export class PlanetListComponent {
   }
 
   ngOnInit(): void {
-    console.log(this.planetName);
   }
 }
